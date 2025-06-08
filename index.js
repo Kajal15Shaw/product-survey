e1cd22606979f7e14bc8a388e145e7af8e566387
Kajal15Shaw <kajalshaw28b@gmail.com>
2025-06-08 14:48:26 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+console.log('Keys:', keys); // Add in index.js or passport.js
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+console.log('Connected to MongoDB URI:', keys.mongoURI);
+
+
+mongoose.connect(keys.mongoURI);
+
+mongoose.connection.on('connected', () => {
+    console.log('✅ Connected to MongoDB');
 });
 
-app.listen(5000);
+mongoose.connection.on('error', (err) => {
+    console.error('❌ MongoDB connection error:', err);
+});
+
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
